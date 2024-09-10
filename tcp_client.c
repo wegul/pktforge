9b1524812f8340c63e3fb9272ecce7791abf29e8
wegul <wegul794@gmail.com>
2024-09-10 18:14:00 -0400

    restore to single-thread

--- tcp_client.c
+++ tcp_client.c
@@ -5,7 +5,6 @@
 #include <arpa/inet.h>
 #include <time.h>
 
-
 #define EPOCH 10
 #define FSIZE 1024 * 1024 * 512 //512MB
 
@@ -13,17 +12,37 @@
 
 char* serv_ip = "10.0.1.4";
 int PORT = 8080;
-const size_t DATA_SIZE = (size_t)FSIZE;
-const size_t SNDBUF_SIZE = 2 * DATA_SIZE;
-uint8_t* data;
-int cur_epoch = 0;
 
 
-int mp_connect() {
+int main(int argc, char* argv[]) {
+    int opt;
+    const size_t DATA_SIZE = (size_t)FSIZE;
+
+    while ((opt = getopt(argc, argv, "s:p:")) != -1) {
+        switch (opt) {
+        case 's':
+            serv_ip = optarg;
+            break;
+        case 'p':
+            char* PORT_chars = optarg;
+            PORT = strtol(PORT_chars, NULL, 10);
+            break;
+        default:
+            break;
+        }
+    }
+
+
+    uint8_t* data = (uint8_t*)malloc(DATA_SIZE);
+    double time_taken, xput, total_time = 0;
+    memset(data, 'A', DATA_SIZE);
+
+
     int sock = 0;
-    struct sockaddr_in serv_addr, cli_addr;
-    socklen_t addr_len = sizeof(cli_addr);
-
+    struct sockaddr_in serv_addr;
+    size_t bytes_sent = 0, total_sent = 0;
+    size_t bytes_received;
+    struct timespec start, end;
 
     if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
         printf("\n Socket creation error \n");
@@ -37,93 +56,39 @@ int mp_connect() {
         printf("\nInvalid address/ Address not supported \n");
         return -1;
     }
+
     //Set send buf
-    setsockopt(sock, SOL_SOCKET, SO_SNDBUF, &SNDBUF_SIZE, sizeof(SNDBUF_SIZE));
+    setsockopt(sock, SOL_SOCKET, SO_SNDBUF, &DATA_SIZE, sizeof(DATA_SIZE));
 
     if (connect(sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) < 0) {
         printf("\nConnection Failed \n");
         return -1;
     }
-    getsockname(sock, (struct sockaddr*)&cli_addr, &addr_len);
-    int client_port = (int)ntohs(cli_addr.sin_port);
-    printf("connected! local port= %d\n", client_port);
     //Warm up
     send(sock, data, DATA_SIZE / 2, 0);
 
-    return sock;
-}
-// Returns bytes sent
-int mp_send(int sock, int flag) {
-    double time_taken, xput;
-
-    struct timespec start, end;
-    size_t bytes_sent = 0;
-
-    clock_gettime(CLOCK_MONOTONIC, &start);
-    bytes_sent = send(sock, data, DATA_SIZE, flag);
-    clock_gettime(CLOCK_MONOTONIC, &end);
-
-    time_taken = (end.tv_sec - start.tv_sec) * 1e9;
-    time_taken = (time_taken + (end.tv_nsec - start.tv_nsec)) * 1e-9;
-
-    xput = (bytes_sent / (1024.0 * 1024.0 * 1024.0)) / time_taken;  // MB/s
-    // printf("Total data sent: %ld MB\n", bytes_sent / (1024 * 1024));
-    // printf("Time taken: %.6f seconds\n", time_taken);
-    // printf("In epoch <%d>, Xput= %.8f Gbps\n", cur_epoch, xput * 8);
-    return bytes_sent;
-}
-
-
-int main(int argc, char* argv[]) {
-    int opt;
-    int nr_conn = 2;
-
-    data = (uint8_t*)malloc(DATA_SIZE);
-    memset(data, 'A', DATA_SIZE);
-
-
-    while ((opt = getopt(argc, argv, "s:n:P:")) != -1) {
-        switch (opt) {
-        case 's':
-            serv_ip = optarg;
-            break;
-        case 'n':
-            char* PORT_chars = optarg;
-            PORT = strtol(PORT_chars, NULL, 10);
-            break;
-        case 'P':
-            nr_conn = strtol(optarg, NULL, 8);
-            break;
-        default:
-            break;
-        }
-    }
-
-
-    pthread_t* threads = (pthread_t*)malloc(nr_conn * sizeof(pthread_t));
-    int* sock_fd = (int*)malloc(nr_conn * sizeof(int));
-    for (size_t i = 0; i < nr_conn; i++) {
-        sock_fd[i] = mp_connect();
-    }
-
+    int cur_epoch = 0;
     while (cur_epoch++ < EPOCH) {
-        int total_sent = 0;
-        double time_taken, xput;
-        struct timespec start, end;
         clock_gettime(CLOCK_MONOTONIC, &start);
-        for (size_t i = 0; i < nr_conn - 1; i++) {
-            total_sent += mp_send(sock_fd[i], 0);//non-blocking
-        }
-        total_sent += mp_send(sock_fd[nr_conn - 1], 0);//blocking
+        bytes_sent = send(sock, data, DATA_SIZE, 0);
         clock_gettime(CLOCK_MONOTONIC, &end);
+
         time_taken = (end.tv_sec - start.tv_sec) * 1e9;
         time_taken = (time_taken + (end.tv_nsec - start.tv_nsec)) * 1e-9;
-        xput = (total_sent / (1024.0 * 1024.0 * 1024.0)) / time_taken;  // MB/s
-        printf("===In epoch <%d>, Average Xput= %.8f Gbps\n", cur_epoch, xput * 8);
-    }
 
-    for (size_t i = 0; i < nr_conn; i++) {
-        close(sock_fd[i]);
+        total_sent += bytes_sent;
+        total_time += time_taken;
+
+        xput = (bytes_sent / (1024.0 * 1024.0 * 1024.0)) / time_taken;  // MB/s
+        // printf("Total data sent: %ld MB\n", bytes_sent / (1024 * 1024));
+        // printf("Time taken: %.6f seconds\n", time_taken);
+        printf("In epoch <%d>, Xput= %.8f Gbps\n", cur_epoch, xput * 8);
     }
+    xput = (total_sent / (1024.0 * 1024.0 * 1024.0)) / total_time;  // MB/s
+    printf("_______\nAvg Xput= %.8f Gbps\n", xput * 8);
+
+
+    close(sock);
+
     return 0;
 }
--- tcp_server.c
+++ tcp_server.c
@@ -3,77 +3,29 @@
 #include <string.h>
 #include <unistd.h>
 #include <fcntl.h>
+#include <time.h>
 #include <arpa/inet.h>
 #include <sys/sendfile.h>
-#include <pthread.h>
 
 #define PORT 8080
-#define BUFFER_SIZE 1024 * 1024 * 64  // 64 MB
+#define BUFFER_SIZE 1024 * 1024 * 512  // 1GB
 
-int mp_receive(int remote_sock) {
+int do_recv() {
+
+}
+
+int main() {
+    const size_t RCVBUF_SIZE = BUFFER_SIZE;
+    int server_fd, new_socket;
+    struct sockaddr_in address;
+    int opt = 1;
+    int addrlen = sizeof(address);
     uint8_t* buffer = (uint8_t*)malloc(BUFFER_SIZE);
+
     struct timespec start, end;
     int bytes_recvd, total_recvd = 0;
     double total_time = 0, time_taken, xput;
-    while (1) {
-        bytes_recvd = 0;
-        clock_gettime(CLOCK_MONOTONIC, &start);
-        bytes_recvd += recv(remote_sock, buffer, BUFFER_SIZE, 0);
-        clock_gettime(CLOCK_MONOTONIC, &end);
-        if (bytes_recvd <= 0) {
-            break;
-        }
-        time_taken = (end.tv_sec - start.tv_sec) * 1e9;
-        time_taken = (time_taken + (end.tv_nsec - start.tv_nsec)) * 1e-9;
-        // xput = (bytes_recvd / (1024.0 * 1024.0 * 1024.0)) / time_taken;  // GB/s
-        // printf("Recv Xput= %.8f Gbps\n", xput * 8);
 
-        total_time += time_taken;
-        total_recvd += bytes_recvd;
-        if (total_recvd >= 1024 * 1024 * 512) {
-            xput = (total_recvd / (1024.0 * 1024.0 * 1024.0)) / total_time;  // GB/s
-            printf("AVG Recv Xput= %.8f Gbps\n", xput * 8);
-            total_time = 0;
-            total_recvd = 0;
-        }
-    }
-    close(remote_sock);
-    free(buffer);
-}
-void* threadFunc(void* vargp) {
-    int* remote_sock = vargp;
-    struct sockaddr_in remote_addr;
-    socklen_t addr_len = sizeof(remote_addr);
-    getpeername(*remote_sock, (struct sockaddr*)&remote_addr, &addr_len);
-
-    // Convert IP addresses from binary to text form
-    char client_ip[INET_ADDRSTRLEN];
-    int client_port;
-    inet_ntop(AF_INET, &(remote_addr.sin_addr), client_ip, INET_ADDRSTRLEN);
-    client_port = (int)ntohs(remote_addr.sin_port);
-    printf("New connection from %s:%d\n", client_ip, client_port);
-    mp_receive(*remote_sock);
-    printf("Finish connection %s:%d\n", client_ip, client_port);
-    return NULL;
-}
-int main(int argc, char* argv[]) {
-    const size_t RCVBUF_SIZE = BUFFER_SIZE;
-    int server_fd;
-    struct sockaddr_in local_addr;
-    int nr_conn = 1, cur_conn = 0;
-    int opt = 1;
-    int addrlen = sizeof(local_addr);
-
-
-    while ((opt = getopt(argc, argv, "P:")) != -1) {
-        switch (opt) {
-        case 'P':
-            nr_conn = strtol(optarg, NULL, 8);
-            break;
-        default:
-            break;
-        }
-    }
 
     // Creating socket file descriptor
     if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
@@ -82,18 +34,18 @@ int main(int argc, char* argv[]) {
     }
 
     // Forcefully attaching socket to the port 8080
-    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &(int){1}, sizeof(int))) {
+    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt))) {
         perror("setsockopt");
         exit(EXIT_FAILURE);
     }
     setsockopt(server_fd, SOL_SOCKET, SO_RCVBUF, &RCVBUF_SIZE, sizeof(RCVBUF_SIZE));
 
-    local_addr.sin_family = AF_INET;
-    local_addr.sin_addr.s_addr = INADDR_ANY;
-    local_addr.sin_port = htons(PORT);
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons(PORT);
 
     // Binding socket to the port 8080
-    if (bind(server_fd, (struct sockaddr*)&local_addr, sizeof(local_addr)) < 0) {
+    if (bind(server_fd, (struct sockaddr*)&address, sizeof(address)) < 0) {
         perror("bind failed");
         exit(EXIT_FAILURE);
     }
@@ -101,28 +53,51 @@ int main(int argc, char* argv[]) {
         perror("listen");
         exit(EXIT_FAILURE);
     }
+    printf("Waiting for connection\n");
 
-    pthread_t* threads = (pthread_t*)malloc(nr_conn * sizeof(pthread_t));
-    while (cur_conn < nr_conn) {
-        struct sockaddr_in remote_addr;
-        int remote_sock;
-        printf("Waiting for connection\n");
+    if ((new_socket = accept(server_fd, (struct sockaddr*)&address, (socklen_t*)&addrlen)) < 0) {
+        perror("accept");
+        exit(EXIT_FAILURE);
+    }
+    // Convert IP addresses from binary to text form
+    char client_ip[INET_ADDRSTRLEN];
+    inet_ntop(AF_INET, &(address.sin_addr), client_ip, INET_ADDRSTRLEN);
+    printf("New connection from %s\n", client_ip);
 
-        if ((remote_sock = accept(server_fd, (struct sockaddr*)&remote_addr, (socklen_t*)&addrlen)) < 0) {
-            perror("accept");
-            exit(EXIT_FAILURE);
+    double total_xput = 0;
+    int counter = 0;
+    while (1) {
+        bytes_recvd = 0;
+        clock_gettime(CLOCK_MONOTONIC, &start);
+        bytes_recvd = recv(new_socket, buffer, BUFFER_SIZE, 0);
+        clock_gettime(CLOCK_MONOTONIC, &end);
+        if (bytes_recvd <= 0) {
+            break;
         }
-
-        // start threading
-        pthread_create(&threads[cur_conn++], NULL, threadFunc, &remote_sock);
+        time_taken = (end.tv_sec - start.tv_sec) * 1e9;
+        time_taken = (time_taken + (end.tv_nsec - start.tv_nsec)) * 1e-9;
+        xput = (bytes_recvd / (1024.0 * 1024.0 * 1024.0)) / time_taken;  // GB/s
+        printf("%.8f \n", xput * 8);
+        total_time += time_taken;
+        total_recvd += bytes_recvd;
+        total_xput += xput;
+        counter++;
+        // if (counter % 100 == 0) {
+        //     xput = (total_recvd / (1024.0 * 1024.0 * 1024.0)) / total_time;  // GB/s
+        //     printf("AVG Recv Xput= %.8f Gbps\n", xput * 8);
+        //     total_time = 0;
+        //     total_recvd = 0;
+        // }
     }
-    for (int i = 0; i < nr_conn; i++) {
-        pthread_join(threads[i], NULL);
-    }
-    printf("________\nFinished all.\n");
+    xput = total_xput / counter;  // GB/s
+    printf("______\n Total AVG Recv Xput= %.8f Gbps\n", xput * 8);
 
+    printf("Finish all\n");
+
+    close(new_socket);
     close(server_fd);
-    free(threads);
+    free(buffer);
+
 
     return 0;
 }
