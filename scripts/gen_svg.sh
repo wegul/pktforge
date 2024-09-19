3d182bd4a495f51a31a211071ccf5b7829c0d1b6
wegul <wegul794@gmail.com>
2024-09-19 18:54:49 -0400

    flamegraph scripts

---
+++ scripts/gen_svg.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# Given a $OUTNAME, extract the perf.data to generate a svg.
+
+FLG_PATH="/home/weigao/FlameGraph"
+
+# Check if the user has provided an argument
+if [ $# -eq 0 ]; then
+    echo "Usage: $0 <output file name.svg>"
+    exit 1
+fi
+OUTNAME=$1
+
+set -e
+
+# Move "perf.data" to "out.perf" in ./data folder
+echo "perf script > ./data/out.perf"
+perf script > ./data/out.perf
+
+# Use flamegraph fold stack
+echo "${FLG_PATH}/stackcollapse-perf.pl ./data/out.perf > ./data/out.folded"
+${FLG_PATH}/stackcollapse-perf.pl ./data/out.perf > ./data/out.folded
+
+# Generate svg
+echo "${FLG_PATH}/flamegraph.pl out.folded > ./data/${OUTNAME}.svg"
+${FLG_PATH}/flamegraph.pl out.folded > ./data/${OUTNAME}.svg
